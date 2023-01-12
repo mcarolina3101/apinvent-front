2da694f5588d0e549c13d9dab9563bd75b651e36
mcarolina3101 <66379342+mcarolina3101@users.noreply.github.com>
2023-01-12 17:12:00 -0500

    add

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,4 +1,4 @@
-export const ruta = "https://"+window.location.hostname+":8443/apinvent/";
-//export const ruta = "http://"+window.location.hostname+":8080/apinvent/";
+//export const ruta = "https://"+window.location.hostname+":8443/apinvent/"; //SERVIDOR BDP
+export const ruta = "http://"+window.location.hostname+":8080/apinvent/";//SERVIDOR LOCAL
 
 export const pageSize =20;
